b19f6af8904dbd7a2ad6399aab65d3166447e550
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2019-12-10 18:18:42 +0100

    added Human class

---
+++ src/creational/builder/Human.java
@@ -0,0 +1,13 @@
+package creational.builder;
+
+public class Human {
+    private String name;
+    private String surname;
+    private int pesel;
+    private String email;
+    private int height;
+    private int weight;
+    private String adress;
+    private String eyeColor;
+    private String hairColor;
+}
