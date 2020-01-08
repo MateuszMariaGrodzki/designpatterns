a78e1450e7a1d0e758e06501c90f166eac594efb
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-01-08 16:33:13 +0100

    added metod implementation from GameLevel class

--- src/structural/adapter/GameLevel.java
+++ src/structural/adapter/GameLevel.java
@@ -1,21 +1,26 @@
 package structural.adapter;
 
 public class GameLevel implements Game {
-    String levelName;
-    int levelNumber;
+    private String levelName;
+    private int levelNumber;
+
+    public GameLevel(String levelName, int levelNumber) {
+        this.levelName = levelName;
+        this.levelNumber = levelNumber;
+    }
 
     @Override
     public void start(String playerName) {
-        
+        System.out.print("Gracz " + playerName + "rozpoczyna poziom " + levelName);
     }
 
     @Override
     public void win() {
-
+        System.out.print("Poziom " + levelNumber + " zostal pomyslnie ukonczony");
     }
 
     @Override
     public void exit() {
-
+        System.out.print("Porazka");
     }
 }
