dd9351aa0f094df41d47c31475c6717a222e1f55
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-01-08 16:44:30 +0100

    added new version of GameLevel class and Game interface

---
+++ src/structural/adapter/GameLevelv2.java
@@ -0,0 +1,28 @@
+package structural.adapter;
+
+import java.sql.SQLOutput;
+
+public class GameLevelv2 implements Gamev2 {
+    private String levelName;
+    private int levelNumber;
+
+    public GameLevelv2(String levelName, int levelNumber) {
+        this.levelName = levelName;
+        this.levelNumber = levelNumber;
+    }
+
+    @Override
+    public void start(String playerName) {
+        System.out.println("Gracz " + playerName + "rozpoczyna poziom " + levelName);
+    }
+
+    @Override
+    public void win(String playerName) {
+        System.out.println("Gracz " + playerName + "przechodzi pomyslnie poziom " + levelNumber);
+    }
+
+    @Override
+    public void lost(String playerName) {
+        System.out.println("Graczowi " + playerName + " nie udaje się przejść poziomu " + levelNumber);
+    }
+}
---
+++ src/structural/adapter/Gamev2.java
@@ -0,0 +1,10 @@
+package structural.adapter;
+
+public interface Gamev2 {
+
+    void start(String playerName);
+
+    void win(String playerName);
+
+    void lost(String playerName);
+}
